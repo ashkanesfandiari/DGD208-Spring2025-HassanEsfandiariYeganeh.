e410deaaab3007c38f6cef0dc9b249214f5ffcfe
hassanesfandiari <es.ashkan7@gmail.com>
2025-05-02 19:54:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DGD208_Spring2025_HassanEsfandiariYeganeh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
